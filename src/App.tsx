eb62f3cd793d73143a1a100054da8e5475ab8058
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-07-05 20:45:40 +0300

    Init board

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-import './App.css';
 import Board from "./components/Board";
 
+import './App.css';
+import BoardModel from "./models/board-model";
+
 function App() {
+  const [board, setBoard] = useState<BoardModel>(new BoardModel());
+
+  const restart = () => {
+    const newBoard = new BoardModel();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
+  useEffect(() => {
+    restart();
+  }, []);
 
   useEffect(() => {
     document.title = 'Chess game';
