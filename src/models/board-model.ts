32e9f7d81866775a50f28a7649209e8b291c3417
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-07-05 20:25:17 +0300

    BoardModel initCells method

---
+++ src/models/board-model.ts
@@ -0,0 +1,19 @@
+import CellModel from "./cell-model";
+
+export default class BoardModel {
+     cells: CellModel[][] = [];
+
+     public initCells() {
+        for (let i = 0; i < 8; i++) {
+            const row: CellModel[] = [];
+            for (let j = 0; j < 8; j++) {
+                if((i + j) % 2 !== 0) {
+                    row.push(new CellModel()); //Black cells
+                } else {
+                    row.push(new CellModel()); //White cells
+                }
+            } 
+            this.cells.push(row);
+        }
+     }
+} 
