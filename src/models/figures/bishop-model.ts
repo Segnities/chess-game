0a0cd66eee98cfa14cfd72a79207c6fa60a0b93e
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-07-10 18:01:16 +0300

    The rules for moving the Bishop figure

--- src/models/figures/bishop-model.ts
+++ src/models/figures/bishop-model.ts
@@ -17,6 +17,9 @@ export default class BishopModel extends FigureModel {
         if(!super.canMove(target)) {
             return false;
         } 
-        return true
+        if(this.cell.isEmptyDiagonal(target)) {
+            return true;
+        }
+        return false;
     }
 }
